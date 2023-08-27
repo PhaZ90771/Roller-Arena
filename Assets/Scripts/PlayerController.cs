1ddf615b9505f6a812e8712d60cbbc226aaad7a0
Benjamin Shutt <shuttben@gmail.com>
2023-08-27 14:18:23 -0700

    Completed Lesson 4.1.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+    public float speed = 5.0f;
+
+    private void Awake()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = FindObjectOfType<RotateCamera>().gameObject;
+    }
+
+    private void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+}
