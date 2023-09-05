3e67b974ecd742648ed51265740ab8fa5389c931
Benjamin Shutt <shuttben@gmail.com>
2023-09-04 19:54:26 -0700

    Updated the Smash Attack force calculation to take into account the distance from the attack area center.

--- Assets/Scripts/SmashArea.cs
+++ Assets/Scripts/SmashArea.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class SmashArea : MonoBehaviour
 {
-    public float SmashAttackStrength = 30.0f;
+    public float SmashAttackStrength = 1f;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Enemy"))
         {
-            var direction = (other.transform.position - transform.position).normalized;
+            var diff = other.transform.position - transform.position;
+            var direction = diff.normalized;
+            var inverseMagnitude = diff.magnitude;
             var enemyRb = other.gameObject.GetComponent<Rigidbody>();
-            enemyRb.AddForce(direction * SmashAttackStrength, ForceMode.Impulse);
+            enemyRb.AddForce(direction * SmashAttackStrength / inverseMagnitude, ForceMode.Impulse);
+            Debug.Log(diff.magnitude + "=>" + SmashAttackStrength / inverseMagnitude);
         }
     }
 }
