02cdef6ce5a56df3271a6300cbca3a2633102cdd
Benjamin Shutt <shuttben@gmail.com>
2023-09-11 20:11:46 -0700

    Setup the powerups to rotate to face the camera.

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -6,6 +6,18 @@ public class Powerup : MonoBehaviour
 {
     public PowerupTypes PowerupType;
 
+    private GameObject focalPoint;
+
+
+    private void Awake()
+    {
+        focalPoint = FindObjectOfType<RotateCamera>().gameObject;
+    }
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(-focalPoint.transform.forward);
+    }
     public enum PowerupTypes
     {
         None,
